41adfbfe96d3133d6e40b7b53816dde6f82e29af
Achraf Dakir <achraf01dakir@gmail.com>
2024-05-11 16:53:28 +0100

    adding components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'enset-app';
+
+  actions : Array<any> = [
+    {title: "Home", "route": "/home", icon: "house"},
+    {title: "Products", "route": "/products", icon: "search"},
+    {title: "New Product", "route": "/newProduct", icon: "plus"}
+  ];
+  currentAction: any;
+  setCurrentAction(action: any) {
+    this.currentAction = action;
+  }
 }
