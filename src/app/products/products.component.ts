83c8aa340474609b968dcbda3dd2a2cb3625aba5
Achraf Dakir <achraf01dakir@gmail.com>
2024-05-12 02:03:15 +0100

    enset app

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ProductService} from "../services/product.service";
+import {Product} from "../model/poduct.model";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products : Array<Product> = [];
 
+  constructor(private productService:ProductService) {
+  }
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe({
+      next : data =>{
+        this.products = data
+      },
+      error : err => {
+        console.log(err)
+      }
+    })
+
+  }
+  handleCheckProduct(product: Product) {
+    this.productService.checkProduct(product).subscribe({
+      next : updatedProduct =>{
+        product.checked =!product.checked;
+        //this.getProducts();
+      }
+    })
+  }
+
+  handleDelete(product: Product) {
+    if (confirm("Are you sure?? .. "))
+      this.productService.deleteProduct(product).subscribe({
+        next:value => {
+          //this.getProducts();
+          this.products= this.products.filter(p=>p.id!=product.id);
+        }
+      })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "../model/poduct.model";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  public getProducts():Observable<Array<Product>>{
+    return this.http.get<Array<Product>>("http://localhost:8089/products");
+  }
+
+  public checkProduct(product:Product):Observable<Product>{
+    return this.http.patch<Product>(`http://localhost:8089/products/${product.id}`,
+      {checked:!product.checked});
+  }
+  public deleteProduct(product:Product){
+    return this.http.delete<any>(`http://localhost:8089/products/${product.id}`);
+  }
+
+  saveProduct(product: Product){
+    return this.http.post<Product>(`http://localhost:8089/products/`,
+      product);
+  }
+
 }
